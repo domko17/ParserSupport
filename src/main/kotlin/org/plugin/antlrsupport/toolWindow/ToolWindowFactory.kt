53e0fe29778cb368d0e777eac4c5fccb5a6a1112
dominikhorvath <dominikhorvath138@gmail.com>
2025-02-15 21:19:27 +0100

    modifications ast to parse tree

--- src/main/kotlin/org/plugin/antlrsupport/toolWindow/ToolWindowFactory.kt
+++ src/main/kotlin/org/plugin/antlrsupport/toolWindow/ToolWindowFactory.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
-import org.plugin.antlrsupport.toolWindow.ast.ASTPanel
+import org.plugin.antlrsupport.toolWindow.tree.ParseTreePanel
 import org.plugin.antlrsupport.toolWindow.atn.ATNPanel
 
 class ToolWindowFactory : ToolWindowFactory, DumbAware {
@@ -15,7 +15,7 @@ class ToolWindowFactory : ToolWindowFactory, DumbAware {
 //        val mainToolWindow = contentFactory.createContent(GrammarLoader(project).getContent(), "ANTLR Grammar", false)
 //        toolWindow.contentManager.addContent(mainToolWindow)
 
-        val parseTreeWindow = contentFactory.createContent(ASTPanel(), "Parse Tree", false)
+        val parseTreeWindow = contentFactory.createContent(ParseTreePanel(), "Parse Tree", false)
         toolWindow.contentManager.addContent(parseTreeWindow)
 
         val atnWindow = contentFactory.createContent(ATNPanel(), "Augmented Transition Networks", false)
--- src/main/kotlin/org/plugin/antlrsupport/toolWindow/tree/ParseTreePanel.kt
+++ src/main/kotlin/org/plugin/antlrsupport/toolWindow/tree/ParseTreePanel.kt
@@ -1,14 +1,14 @@
-package org.plugin.antlrsupport.toolWindow.ast
+package org.plugin.antlrsupport.toolWindow.tree
 
 import com.intellij.ui.components.JBPanel
 import javax.swing.JLabel
 import javax.swing.JPanel
 
-class ASTPanel : JBPanel<ASTPanel>() {
+class ParseTreePanel : JBPanel<ParseTreePanel>() {
     private val content: JPanel = JPanel()
 
     init {
-        content.add(JLabel("Hello ASTPanel!"))
+        content.add(JLabel("Hello parse tree Panel!"))
         add(content)
     }
 }
