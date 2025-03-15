2fc5ba90bbe8de564572d7555a4ba015c0f686bd
dominikhorvath <dominikhorvath138@gmail.com>
2025-03-15 23:19:51 +0100

    create list grammar with text field for selected parser path

--- src/main/kotlin/org/plugin/antlrsupport/toolWindow/ParserListPanel.kt
+++ src/main/kotlin/org/plugin/antlrsupport/toolWindow/ParserListPanel.kt
@@ -1,16 +1,16 @@
 package org.plugin.antlrsupport.toolWindow
 
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 import com.intellij.ui.components.JBList
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.components.JBScrollPane
-import org.plugin.antlrsupport.AntlrParserGenerator
-import org.plugin.antlrsupport.notify
+import org.plugin.antlrsupport.GrammarFileTrackerService
 import java.awt.BorderLayout
-import javax.swing.BoxLayout
-import javax.swing.JButton
-import javax.swing.JPanel
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
+import java.io.File
+import javax.swing.*
+
 
 /**
  * Created by Dominik.
@@ -22,13 +22,32 @@ class ParserListPanel(project: Project) : JBPanel<ParserListPanel>() {
     private val content: JPanel = JPanel()
 
     init {
-//        val grammarList = JBList((0 until grammarTrackerService.listModel.size()).map { grammarTrackerService.listModel.getElementAt(it).name })
-//        val grammarList = JBList()
-//        val panel = JPanel(BorderLayout())
-//        panel.add(JBScrollPane(grammarList), BorderLayout.CENTER)
+        val grammarTrackerService = project.getService(GrammarFileTrackerService::class.java)
+        grammarTrackerService.listModel
+        val grammarList = JBList((0 until grammarTrackerService.listModel.size()).map { grammarTrackerService.listModel.getElementAt(it).name })
 
+        val panel = JPanel(BorderLayout())
+        // updated grammar list after created or removed grammar file
+        grammarTrackerService.addUpdateListener {
+            grammarList.updateUI()
+        }
 
-//        content.add(panel)
+        val filePathField = JTextField(20)
+        filePathField.isEditable = false;
+        val browseButton = JButton("Browse")
+        browseButton.addActionListener {
+            val fileChooser = JFileChooser()
+            val returnValue = fileChooser.showOpenDialog(panel)
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                val selectedFile: File = fileChooser.selectedFile
+                filePathField.text = selectedFile.getAbsolutePath()
+            }
+        }
+
+        panel.add(JBScrollPane(grammarList), BorderLayout.WEST)
+        panel.add(filePathField, BorderLayout.CENTER)
+        panel.add(browseButton, BorderLayout.EAST)
+        content.add(panel)
         add(content)
     }
 }
